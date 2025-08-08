b0a0262f2601db165ae7329bc8a95f8141732a3e
Sinclair <sinclair.rockwell-kollmann@pharmazie.uni-freiburg.de>
2025-08-08 18:07:19 +0200

    seperate concerns, add dependence on global env variables and fix small error.

--- slurm/feature_extraction/move_node_to_bench.sh
+++ slurm/feature_extraction/move_node_to_bench.sh
@@ -2,37 +2,43 @@
 
 # move output to bench
 # check if output is written to project
-FEATURES=/pfs/10/project/bw16k010/benchmark/features
-HISTAI=${FEATURES}/histai/output
-COBRA=${FEATURES}/cobra/output
-PP=${FEATURES}/pp/output
+FEATURES="${BENCH}/features"
+HISTAI=${TMPDIR}/histai/output
+COBRA=${TMPDIR}/cobra/output
+PP=${TMPDIR}/pp/output
 
 # for histai
 if [ -d "$HISTAI" ] && find "$HISTAI" -type f -name '*.h5' | grep -q .; then
     echo "Folder ${HISTAI} exists and contains .h5 files"
+    mkdir -p "${FEATURES}/histai"
+    cp -r "${HISTAI}/*" "${FEATURES}/histai/"
 else
     echo "Folder ${HISTAI} does not exist or contains no .h5 files"
     archive_name="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
-    tar -czf "$archive_name" -C "$(dirname "${TMPDIR}/histai/output")" "$(basename "${TMPDIR}/histai/output")"
+    tar -czf "$archive_name" -C "$(dirname "${HISTAI}")" "$(basename "${HISTAI}")"
     mv "$archive_name" "$FEATURES"
 fi
 
 # for cobra
 if [ -d "$COBRA" ] && find "$COBRA" -type f -name '*.h5' | grep -q .; then
     echo "Folder ${COBRA} exists and contains .h5 files"
+    mkdir -p "${FEATURES}/cobra"
+    cp -r "${COBRA}/*" "${FEATURES}/cobra/"
 else
     echo "Folder ${COBRA} does not exist or contains no .h5 files"
     archive_name="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
-    tar -czf "$archive_name" -C "$(dirname "${TMPDIR}/cobra/output")" "$(basename "${TMPDIR}/cobra/output")"
+    tar -czf "$archive_name" -C "$(dirname "${COBRA}")" "$(basename "${COBRA}")"
     mv "$archive_name" "$FEATURES"
 fi
 
 # for pp
 if [ -d "$PP" ] && find "$PP" -type f -name '*.h5' | grep -q .; then
     echo "Folder ${PP} exists and contains .h5 files"
+    mkdir -p "${FEATURES}/pp"
+    cp -r "${PP}/*" "${FEATURES}/pp/"
 else
     echo "Folder ${PP} does not exist or contains no .h5 files"
     archive_name="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
-    tar -czf "$archive_name" -C "$(dirname "${TMPDIR}/pp/output")" "$(basename "${TMPDIR}/pp/output")"
+    tar -czf "$archive_name" -C "$(dirname "${PP}")" "$(basename "${PP}")"
     mv "$archive_name" "$FEATURES"
 fi
