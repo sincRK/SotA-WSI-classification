c5ddf1975857cc63166f420b598496385a9556ed
Sinclair <sinclair.rockwell-kollmann@pharmazie.uni-freiburg.de>
2025-08-08 16:31:49 +0200

    add usage description

--- slurm/feature_extraction/copy_from_manifest.sh
+++ slurm/feature_extraction/copy_from_manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Usage: 
+# Usage: copy_from_manifest.sh /path/manifest.csv /path/target_dir
+
 manifest="$1"
 target_dir="$2"
 
@@ -14,46 +15,12 @@ if [ ! -f "$manifest" ]; then
     return 2
 fi
 
-manifest_dir="$(cd "$(dirname "$manifest")" && pwd)"
-
 mkdir -p "$target_dir"
-
-echo "Manifest dir ${manifest_dir}"
 
 tail -n +2 "$manifest" | while IFS=',' read -r target_filename original_path; do
 
     original_path_abs=$(realpath "$original_path")
-    overlap=""
-
-    IFS='/' read -r -a parts1 <<< "$manifest_dir"
-    IFS='/' read -r -a parts2 <<< "$original_path_abs"
-    for ((i=0; i<${#parts1[@]} && i<${#parts2[@]}; i++)); do
-    if [[ "${parts1[i]}" == "${parts2[i]}" ]]; then
-            overlap="${overlap}/${parts1[i]}"
-        else
-            break
-        fi
-    done
-
-    # Remove double slashes if any
-    overlap=$(echo "$overlap" | sed 's#//*#/#g')
-
-    echo "Overlap ${overlap}"
-
-    if [[ -n "$overlap" ]]; then
-        # Absolute Pfadangabe basierend auf Manifest-Verzeichnis
-        overlap_len=${#overlap}
-        joined="${manifest_dir}${original_path_abs:$overlap_len}"
-    else
-        joined="${manifest_dir}/${original_path_abs}"
-    fi
-
-    if [ ! -f "$joined" ]; then
-        echo "Warning: File not found: $joined"
-        continue
-    fi
-
-    cp "$joined" "$target_dir/$target_filename"
+    cp "$original_path_abs" "$target_dir/$target_filename"
 done
 
 echo "Copy completed to: $target_dir"
