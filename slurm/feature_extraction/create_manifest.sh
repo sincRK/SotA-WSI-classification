c8bbd4faeb302aaf8737222dd5064c8247bf25a1
Sinclair <sinclair.rockwell-kollmann@pharmazie.uni-freiburg.de>
2025-08-08 16:17:04 +0200

    add usage description

--- slurm/feature_extraction/create_manifest.sh
+++ slurm/feature_extraction/create_manifest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Usage create_manifest.sh /path/image_folder image_ext
+# manifest.csv will be created in same folder as $(pwd)
+# the file names in manifest.csv will be relative to /path/image
+# and will start with _path_image_folder_...
+
 # Ausgangsverzeichnis
 src_root="${1:-.}"  # kann per Argument überschrieben werden
 file_type="${2:-tiff}"
@@ -8,7 +13,7 @@ manifest="manifest.csv"
 # Manifest initialisieren
 echo "target_filename,original_path" > "$manifest"
 
-# Alle .tiff-Dateien im Quellverzeichnis rekursiv durchlaufen
+# Alle .file_type-Dateien im Quellverzeichnis rekursiv durchlaufen
 find "$src_root" -type f -name "*.${file_type}" | while read -r filepath; do
     # relativer Pfad ohne führendes foo/
     rel_path="${filepath#$src_root/}"
