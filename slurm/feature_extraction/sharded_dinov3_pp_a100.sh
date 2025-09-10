bc1006102a9710af90ce8b366ba1eb13986bcff2
Sinclair <sinclair.rockwell-kollmann@pharmazie.uni-freiburg.de>
2025-09-10 23:56:58 +0200

    adapt gpu resources for isyntax

--- slurm/feature_extraction/sharded_dinov3_pp_a100.sh
+++ slurm/feature_extraction/sharded_dinov3_pp_a100.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=150000m
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=sinclair.rockwell-kollmann@pharmazie.uni-freiburg.de
-#SBATCH --ntasks=10
-#SBATCH --cpus-per-task=5
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:a100:1
 #SBATCH --partition=gpu
 
@@ -53,14 +53,14 @@ bash ${TMPDIR}/feature_extraction/rewrite_trident_ckpts.sh $INPUT_JSON $INPUT_JS
 which python
 
 # conch_v15 - 2:30h x 4
-bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 3 18 feat $PATCH_ENCODER 20 512 128 ${TMPDIR}/${FEAT_DIR}/20x_512px_0px_overlap/
+bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 1 18 feat $PATCH_ENCODER 20 512 128 ${TMPDIR}/${FEAT_DIR}/20x_512px_0px_overlap/
 rsync -av ${TMPDIR}/${FEAT_DIR} $BENCH
 
-bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 3 18 feat $PATCH_ENCODER 20 256 128 ${TMPDIR}/${FEAT_DIR}/20x_256px_0px_overlap/
+bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 1 18 feat $PATCH_ENCODER 20 256 128 ${TMPDIR}/${FEAT_DIR}/20x_256px_0px_overlap/
 rsync -av ${TMPDIR}/${FEAT_DIR} $BENCH
 
-bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 3 18 feat $PATCH_ENCODER 10 512 128 ${TMPDIR}/${FEAT_DIR}/10x_512px_0px_overlap/
+bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 1 18 feat $PATCH_ENCODER 10 512 128 ${TMPDIR}/${FEAT_DIR}/10x_512px_0px_overlap/
 rsync -av ${TMPDIR}/${FEAT_DIR} $BENCH
 
-bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 3 18 feat $PATCH_ENCODER 10 256 128 ${TMPDIR}/${FEAT_DIR}/10x_256px_0px_overlap/
+bash ${TMPDIR}/feature_extraction/feature_extraction_sharded.sh $DATA_DIR ${TMPDIR}/${FEAT_DIR} 1 18 feat $PATCH_ENCODER 10 256 128 ${TMPDIR}/${FEAT_DIR}/10x_256px_0px_overlap/
 rsync -av ${TMPDIR}/${FEAT_DIR} $BENCH
